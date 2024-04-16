6f1a43d6b103a45107b1c83cd06bcd5a2ad7fe76
ehcaw <ryannguyenc@gmail.com>
2024-04-16 15:10:25 -0700

    replaced boilerplate code

--- index.js
+++
@@ -1,7 +0,0 @@
-// index.js
-import React from "react";
-import ReactDOM from "react-dom";
-import SidePanel from "./sidepanel.jsx";
-import "./global.css";
-
-ReactDOM.render(<SidePanel></SidePanel>, document.getElementById("root"));
